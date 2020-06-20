bd7a1bafb9903e5f9a169587c3f464628517d79e
Armando Alaniz <arjoalaniz@gmail.com>
2020-06-20 11:25:59 -0600

    Convertir tab a espacio en test de integracion

--- attendance/src/test/java/ni/org/jug/hr/attendance/AttendanceApplicationTests.java
+++ attendance/src/test/java/ni/org/jug/hr/attendance/AttendanceApplicationTests.java
@@ -8,72 +8,73 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class AttendanceApplicationTests {
 
-	@PersistenceContext
-	EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
-	@Test
-	void fetchOnlyHeader() {
-		TypedQuery<Attendance> query = em.createQuery("SELECT a FROM Attendance a WHERE a.year >= 2019", Attendance.class);
+    @Test
+    void fetchOnlyHeader() {
+        TypedQuery<Attendance> query = em.createQuery("SELECT a FROM Attendance a WHERE a.year >= 2019", Attendance.class);
 
-		long start = System.currentTimeMillis();
-		List<Attendance> data = query.getResultList();
-		System.out.println("Days: " + data.size());
-		for (Attendance att : data) {
-			System.out.println("Date: " + att.getDate() + ", dayname: " + att.getDayname());
-		}
-		System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
-	}
+        long start = System.currentTimeMillis();
+        List<Attendance> data = query.getResultList();
+        System.out.println("Days: " + data.size());
+        for (Attendance att : data) {
+            System.out.println("Date: " + att.getDate() + ", dayname: " + att.getDayname());
+        }
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
+    }
 
-	@Test
-	@Transactional
-	void fetchHeaderAndLazyLoadDetail() {
-		TypedQuery<Attendance> query = em.createQuery("SELECT a FROM Attendance a WHERE a.year >= 2019", Attendance.class);
+    @Test
+    @Transactional
+    void fetchHeaderAndLazyLoadDetail() {
+        TypedQuery<Attendance> query = em.createQuery("SELECT a FROM Attendance a WHERE a.year >= 2019", Attendance.class);
 
-		long start = System.currentTimeMillis();
-		List<Attendance> data = query.getResultList();
-		System.out.println("Days: " + data.size());
-		for (Attendance att : data) {
-			int count = att.getDetails().size();
-			System.out.println("Date: " + att.getDate() + ", dayname: " + att.getDayname() + ", employee count: " + count);
-		}
-		System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
-	}
+        long start = System.currentTimeMillis();
+        List<Attendance> data = query.getResultList();
+        System.out.println("Days: " + data.size());
+        for (Attendance att : data) {
+            int count = att.getDetails().size();
+            System.out.println("Date: " + att.getDate() + ", dayname: " + att.getDayname() + ", employee count: " + count);
+        }
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
+    }
 
-	@Test
-	void fetchHeaderAndDetail() {
-		TypedQuery<Attendance> query = em.createQuery("SELECT DISTINCT a FROM Attendance a JOIN FETCH a.details d " +
-				"WHERE a.year >= 2019", Attendance.class);
+    @Test
+    void fetchHeaderAndDetail() {
+        TypedQuery<Attendance> query = em.createQuery("SELECT DISTINCT a FROM Attendance a JOIN FETCH a.details d " +
+                "WHERE a.year >= 2019", Attendance.class);
 
-		long start = System.currentTimeMillis();
-		List<Attendance> data = query.getResultList();
-		System.out.println("Days: " + data.size());
-		for (Attendance att : data) {
-			int count = att.getDetails().size();
-			System.out.println("Date: " + att.getDate() + ", dayname: " + att.getDayname() + ", employee count: " + count);
-		}
-		System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
-	}
+        long start = System.currentTimeMillis();
+        List<Attendance> data = query.getResultList();
+        System.out.println("Days: " + data.size());
+        for (Attendance att : data) {
+            int count = att.getDetails().size();
+            System.out.println("Date: " + att.getDate() + ", dayname: " + att.getDayname() + ", employee count: " + count);
+        }
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
+    }
 
-	@Test
-	void fetchRequiredData() {
-		TypedQuery<CountDTO> query = em.createQuery("SELECT new ni.org.jug.hr.attendance.model.CountDTO(a.date, a.dayname, COUNT(d)) " +
-				"FROM Attendance a JOIN a.details d " +
-				"WHERE a.year >= 2019 " +
-				"GROUP BY a.date, a.dayname", CountDTO.class);
+    @Test
+    void fetchProjectionUsingDTO() {
+        TypedQuery<CountDTO> query = em.createQuery("SELECT new ni.org.jug.hr.attendance.model.CountDTO(a.date, a.dayname, COUNT(d)) " +
+                "FROM Attendance a JOIN a.details d " +
+                "WHERE a.year >= 2019 " +
+                "GROUP BY a.date, a.dayname", CountDTO.class);
 
-		long start = System.currentTimeMillis();
-		List<CountDTO> data = query.getResultList();
-		System.out.println("Days: " + data.size());
-		for (CountDTO att : data) {
-			Long count = att.count;
-			System.out.println("Date: " + att.date + ", dayname: " + att.dayName + ", employee count: " + count);
-		}
-		System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
-	}
+        long start = System.currentTimeMillis();
+        List<CountDTO> data = query.getResultList();
+        System.out.println("Days: " + data.size());
+        for (CountDTO att : data) {
+            System.out.println("Date: " + att.date + ", dayname: " + att.dayName + ", employee count: " + att.count);
+        }
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
+    }
 }
