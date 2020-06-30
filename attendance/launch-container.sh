2acb3a36720e97ab67dc80dcc89af38997175958
Armando Alaniz <arjoalaniz@gmail.com>
2020-06-29 18:19:09 -0600

    Usar tmpfs para montar el volumen del contenedor de postgres

--- attendance/launch-container.sh
+++ attendance/launch-container.sh
@@ -7,9 +7,6 @@ DOCKER_EMOJI="\xf0\x9f\x90\xb3"
 echo -e "${DOCKER_EMOJI} ${YELLOW}Build image${RESET}"
 docker image build -t pg-payroll:1 .
 
-echo -e "${DOCKER_EMOJI} ${YELLOW}Create volume${RESET}"
-docker volume create payroll-data
-
 CONTAINER_ID=$(docker container ls --filter name=pg-payroll -q)
 if [[ -n "$CONTAINER_ID" ]]; then
   echo -e "${DOCKER_EMOJI} ${YELLOW}Stop and remove running container:${RESET} ${CONTAINER_ID}"
@@ -17,4 +14,4 @@ if [[ -n "$CONTAINER_ID" ]]; then
 fi
 
 echo -e "${DOCKER_EMOJI} ${YELLOW}Create container${RESET}"
-docker container run --name pg-payroll -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres -v payroll-data:/var/lib/postgresql/data pg-payroll:1
+docker container run --name pg-payroll -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --tmpfs /var/lib/postgresql/data pg-payroll:1
